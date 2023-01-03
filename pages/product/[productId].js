e0e14360d086aa121ac105a4e1dd6730994c817c
KevinArce <iav.kevinarce@ufg.edu.sv>
2023-01-02 19:37:51 -0600

    first commit

---
+++ pages/product/[productId].js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useRouter } from "next/router";
+
+const ProductItem = () => {
+  const router = useRouter();
+  const { productId } = router.query;
+  return (
+    <div>
+      <h1>Product Item: {productId}</h1>
+    </div>
+  );
+};
+
+export default ProductItem;
